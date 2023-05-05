40346b51107221a20ac02948e3523b631b6186ba
XiangYu <49023462+xiangyu5632@users.noreply.github.com>
2023-05-05 11:29:09 +0800

    update InfluxDBFactory.java
    support config muti ip

--- src/main/java/org/influxdb/InfluxDBFactory.java
+++ src/main/java/org/influxdb/InfluxDBFactory.java
@@ -6,6 +6,8 @@ import org.influxdb.impl.InfluxDBImpl;
 import okhttp3.OkHttpClient;
 import org.influxdb.impl.Preconditions;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -27,9 +29,21 @@ public enum InfluxDBFactory {
    */
   public static InfluxDB connect(final String url) {
     Preconditions.checkNonEmptyString(url, "url");
-    return new InfluxDBImpl(url, null, null, new OkHttpClient.Builder());
+    return new InfluxDBImpl(Arrays.asList(url), null, null, new OkHttpClient.Builder());
   }
 
+  /**
+   * Create a connection to a InfluxDB.
+   *
+   * @param urls 
+                the url to connect to.
+   * @return a InfluxDB adapter suitable to access a InfluxDB.
+   */
+  public static InfluxDB connect(final List<String> urls) {
+        Preconditions.checkNonEmptyString(urls, "urls");
+        return new InfluxDBImpl(urls, null, null, new OkHttpClient.Builder());
+  }
+  
   /**
    * Create a connection to a InfluxDB.
    *
@@ -45,9 +59,26 @@ public enum InfluxDBFactory {
   public static InfluxDB connect(final String url, final String username, final String password) {
     Preconditions.checkNonEmptyString(url, "url");
     Preconditions.checkNonEmptyString(username, "username");
-    return new InfluxDBImpl(url, username, password, new OkHttpClient.Builder());
+    return new InfluxDBImpl(Arrays.asList(url), username, password, new OkHttpClient.Builder());
   }
-
+  
+  /**
+   * Create a connection to a InfluxDB.
+   *
+   * @param urls     
+                the urls to connect to.
+   * @param username 
+                the username which is used to authorize against the influxDB instance.
+   * @param password 
+                the password for the username which is used to authorize against the influxDB instance.
+   * @return a InfluxDB adapter suitable to access a InfluxDB.
+   */
+  public static InfluxDB connect(final List<String> urls, final String username, final String password) {
+        Preconditions.checkNonEmptyString(urls, "urls");
+        Preconditions.checkNonEmptyString(username, "username");
+        return new InfluxDBImpl(urls, username, password, new OkHttpClient.Builder());
+  }
+  
   /**
    * Create a connection to a InfluxDB.
    *
@@ -60,8 +91,22 @@ public enum InfluxDBFactory {
   public static InfluxDB connect(final String url, final OkHttpClient.Builder client) {
     Preconditions.checkNonEmptyString(url, "url");
     Objects.requireNonNull(client, "client");
-    return new InfluxDBImpl(url, null, null, client);
+    return new InfluxDBImpl(Arrays.asList(url), null, null, client);
   }
+  
+  /**
+   * Create a connection to a InfluxDB.
+   *
+   * @param urls   the urls to connect to.
+   * @param client the HTTP client to use
+   * @return a InfluxDB adapter suitable to access a InfluxDB.
+   */
+  public static InfluxDB connect(final List<String> urls, final OkHttpClient.Builder client) {
+        Preconditions.checkNonEmptyString(urls, "urls");
+        Objects.requireNonNull(client, "client");
+        return new InfluxDBImpl(urls, null, null, client);
+  }
+  
 
   /**
    * Create a connection to a InfluxDB.
@@ -80,6 +125,21 @@ public enum InfluxDBFactory {
   public static InfluxDB connect(final String url, final String username, final String password,
       final OkHttpClient.Builder client) {
     return connect(url, username, password, client, ResponseFormat.JSON);
+  }
+  
+  /**
+   * Create a connection to a InfluxDB.
+   *
+   * @param urls     the urls to connect to.
+   * @param username the username which is used to authorize against the influxDB instance.
+   * @param password the password for the username which is used to authorize against the influxDB
+   *                 instance.
+   * @param client   the HTTP client to use
+   * @return a InfluxDB adapter suitable to access a InfluxDB.
+   */
+  public static InfluxDB connect(final List<String> urls, final String username, final String password,
+                                   final OkHttpClient.Builder client) {
+    return connect(urls, username, password, client, ResponseFormat.JSON);
   }
 
   /**
@@ -103,6 +163,25 @@ public enum InfluxDBFactory {
     Preconditions.checkNonEmptyString(url, "url");
     Preconditions.checkNonEmptyString(username, "username");
     Objects.requireNonNull(client, "client");
-    return new InfluxDBImpl(url, username, password, client, responseFormat);
+    return new InfluxDBImpl(Arrays.asList(url), username, password, client, responseFormat);
+  }
+  
+  /**
+   * Create a connection to a InfluxDB.
+   *
+   * @param urls           the urls to connect to.
+   * @param username       the username which is used to authorize against the influxDB instance.
+   * @param password       the password for the username which is used to authorize against the influxDB
+   *                       instance.
+   * @param client         the HTTP client to use
+   * @param responseFormat The {@code ResponseFormat} to use for response from InfluxDB server
+   * @return a InfluxDB adapter suitable to access a InfluxDB.
+   */
+  public static InfluxDB connect(final List<String> urls, final String username, final String password,
+                                   final OkHttpClient.Builder client, final ResponseFormat responseFormat) {
+    Preconditions.checkNonEmptyString(urls, "urls");
+    Preconditions.checkNonEmptyString(username, "username");
+    Objects.requireNonNull(client, "client");
+    return new InfluxDBImpl(urls, username, password, client, responseFormat);
   }
 }
